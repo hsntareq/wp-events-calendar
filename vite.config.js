03be45d1f0a40238c2ff44cae145611f724a3080
Hasan Tareq <hsntareq@gmail.com>
2024-05-03 14:53:53 +0600

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,23 @@ export default defineConfig({
 	build: {
 		// generate .vite/manifest.json in outDir
 		manifest: true,
-		assetsDir: 'dist',
-		outDir: 'public',
+		assetsDir: './',
+		outDir: 'dist',
 		rollupOptions: {
 			// overwrite default .html entry
-			input: [
-				'/assets/js/main.js',
-				'/assets/scss/index.scss',
-			],
-			// output: {
-			// 	// overwrite default .html entry
-			// 	entryFileNames: '[name].js',
-			// 	assetFileNames: '[name][extname]',
-			// 	chunkFileNames: '[name].js',
-			// },
+			input: {
+				'admin': '/assets/src/admin.js',
+				'front': '/assets/src/admin.scss'
+			},
+			output: {
+				'admin.js': '/assets/js/admin.min.js',
+				'front.js': '/assets/js/front.min.js',
+
+				// overwrite default .html entry
+				// entryFileNames: '[name].js',
+				// assetFileNames: '[name][extname]',
+				// chunkFileNames: '[name].js',
+			},
 		},
 	},
 
